5911018bd7d8af50ec20ade29019047f8a30767d
schulzy@freemail.hu <schulzy@freemail.hu>
2018-04-12 22:36:17 +0200

    Chat server initialization

---
+++ Schulzy.ChatApp/Schulzy.ChatServerDotNetCore/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNet.SignalR;
+using Microsoft.Owin.Cors;
+using Microsoft.Owin.Hosting;
+using Owin;
+
+namespace Schulzy.ChatServerDotNetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var url = "http://localhost:8080/";
+            using (WebApp.Start<Startup>(url))
+            {
+                Console.WriteLine($"Server running at {url}");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Schulzy.ChatApp/Schulzy.ChatServerDotNetCore/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.AspNet.SignalR;
+using Microsoft.Owin.Cors;
+using Microsoft.Owin.Hosting;
+using Owin;
+
+namespace Schulzy.ChatServerDotNetCore
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCors(CorsOptions.AllowAll);
+            app.MapSignalR("/signalchat", new HubConfiguration());
+        }
+    }
+}

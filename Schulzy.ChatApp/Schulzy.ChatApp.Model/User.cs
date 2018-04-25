550fb70792ac5fd266c4f6b1ceb08fe689f6e1dc
schulzy@freemail.hu <schulzy@freemail.hu>
2018-04-25 22:31:51 +0200

    ChatHub has been added

---
+++ Schulzy.ChatApp/Schulzy.ChatApp.Model/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Schulzy.ChatApp.Model
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string ID { get; set; }
+        public byte[] Photo { get; set; }
+    }
+}
---
+++ Schulzy.ChatApp/Schulzy.ChatServer/IClient.cs
@@ -0,0 +1,15 @@
+﻿using Schulzy.ChatApp.Model;
+namespace Schulzy.ChatServer
+{
+    public interface IClient
+    {
+        void ParticipantDisconnection(string name);
+        void ParticipantReconnection(string name);
+        void ParticipantLogin(User client);
+        void ParticipantLogout(string name);
+        void BroadcastTextMessage(string sender, string message);
+        void BroadcastPictureMessage(string sender, byte[] img);
+        void UnicastTextMessage(string sender, string message);
+        void UnicastPictureMessage(string sender, byte[] img);
+    }
+}
